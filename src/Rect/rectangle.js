5554b00c93816e55dd0dc468d2a18db9eff20563
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2020-08-06 12:27:23 +1200

    Made a rectangle for your parking

---
+++ src/Rect/rectangle.js
@@ -0,0 +1,13 @@
+ function setup(){
+     createCanvas(400, 600);
+}
+ function draw(){
+    // clear background
+    background(200);
+    // colour
+    stroke('red'); 
+    //thickness
+    strokeWeight(6);
+     //A rectangle
+    rect (40, 120, 120, 40, 4);
+ }
