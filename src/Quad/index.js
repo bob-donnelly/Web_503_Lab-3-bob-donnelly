ee2f5b9a2383dae6f3f2bb782892d20b262762ff
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2020-08-08 15:34:03 +1200

    made a quad rotated it 35 degrees and gave it a colour of blue

--- src/Quad/index.js
+++ src/Quad/index.js
@@ -1,4 +1,19 @@
 function setup (){
     createCanvas(800, 600);
-    
+}
+
+function draw (){
+    //background colour 
+    background('200');
+    fill(125, 80, 220);
+    //stroke 
+    noStroke();
+    //this value moves the shape to the center of the canvas
+    translate(width / 2, height / 2);
+    //rotates the angle by the axis
+    rotate(35,0);
+    //quad dimensions 8 sides
+    quad(45, 39, 95, 30, 75, 70, 50, 92);
+    //stroke thickness of 0
+    strokeweight (0);   
 }
